a6a376f3845738e8eb331469411bef40743a4453
captainearth1 <61050893@kmitl.ac.th>
2020-10-08 22:22:20 +0700

    atm-spring-code-config

--- src/main/java/KMITL/atm/Main.java
+++ src/main/java/KMITL/atm/Main.java
@@ -7,15 +7,10 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context =
-                new AnnotationConfigApplicationContext(ATMConfig.class);
-
-
-
+        new AnnotationConfigApplicationContext(ATMConfig.class);
         AtmUI atmUI = context.getBean(AtmUI.class);
         atmUI.run();
 
-        /*String name = atm.validateCustomer(1,1234);
-        System.out.println(name + "has" +atm.getBalance());*/
 
 
     }
